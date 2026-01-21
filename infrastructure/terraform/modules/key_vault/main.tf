a3567b2a04ca054e95686d8dc126f890fb29e762
Amanda Souza <aminueza@users.noreply.github.com>
2026-01-21 21:12:50 +0100

    chore(terraform): add lifecycle block to Key Vault role assignment to ignore changes to principal_id

--- infrastructure/terraform/modules/key_vault/main.tf
+++ infrastructure/terraform/modules/key_vault/main.tf
@@ -84,4 +84,8 @@ resource "azurerm_role_assignment" "terraform_secrets_officer" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = data.azurerm_client_config.current.object_id
+
+  lifecycle {
+    ignore_changes = [principal_id]
+  }
 }
